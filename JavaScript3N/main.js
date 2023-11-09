83af0f92215d1dcec9738ea2655a1b78583afdf7
Hériky C Lopes <130413477+HerikyDev3N@users.noreply.github.com>
2023-11-08 21:47:42 -0300

    Add files via upload

---
+++ JavaScript3N/main.js
@@ -0,0 +1,31 @@
+/*const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Qual é a sua nota? ', (resposta) => {
+  // Convertemos a resposta em um número para comparar corretamente.
+  const nota = parseFloat(resposta);
+
+  if (!isNaN(nota)) { // Verificamos se a entrada é um número válido.
+    if (nota >= 6) {
+      console.log("APROVADO");
+    } else if (nota < 4) {
+      console.log("REPROVADO");
+    } else {
+      console.log("RECUPERAÇÃO");
+    }
+  } else {
+    console.log("Por favor, insira uma nota válida.");
+  }
+
+  rl.close();
+});*/
+
+  
+
+
+
+
